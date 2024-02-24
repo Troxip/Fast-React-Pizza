bf9443d1c77dc5f13a2c6ab8e85a582be5319ad5
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-02-24 18:54:42 +0100

    Username

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux";
 export default function Username() {
   const username = useSelector((state) => state.user.username);
 
+  if (!username) return;
+
   return (
     <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
