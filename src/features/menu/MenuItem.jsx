171e0ca7313e2e93b0a822078c6ef1596781c607
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-02-24 22:47:17 +0100

    Submit

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import { formatCurrency } from "../../utils/helpers";
 import Button from "./../../ui/Button";
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteItemBtn from "../cart/DeleteItemBtn";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -43,7 +44,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isInCart && <DeleteItemBtn pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItemBtn pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
